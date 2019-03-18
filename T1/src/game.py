b3c37c29d43cb1748370451bfaa215b8ea40a126
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-03-18 19:52:55 +0000

    Adapting aima's Problem class

--- T1/src/game.py
+++ T1/src/game.py
@@ -279,3 +279,240 @@ class BloxorzProblem(Problem):
     
     def __init__(self, initial, goal):
         Problem.__init__(self, initial, goal)
+
+    def actions(self, state):
+        possibleActions = ['Up', 'Down', 'Left', 'Right']
+
+        for action in possibleActions:
+            if not self.validate(action, state):
+                possibleActions.remove(action)
+
+        return possibleActions
+
+    def resultUp(self, blockCoords):
+
+        if blockCoords[4] == "V":
+            blockCoords[0] -= 2
+            blockCoords[2] -= 1
+            blockCoords[4] = "H"
+        elif blockCoords[4] == "H":
+            if blockCoords[0] == blockCoords[2]:
+                blockCoords[0] -= 1
+                blockCoords[2] -= 1
+            else:
+                blockCoords[0] -= 1
+                blockCoords[2] -= 2
+                blockCoords[4] = "V"
+
+        return blockCoords
+
+    def resultDown(self, blockCoords):
+
+        if blockCoords[4] == "V":
+            blockCoords[0] += 1
+            blockCoords[2] += 2
+            blockCoords[4] = "H"
+        elif blockCoords[4] == "H":
+            if blockCoords[0] == blockCoords[2]:
+                blockCoords[0] += 1
+                blockCoords[2] += 1
+            else:
+                blockCoords[0] += 2
+                blockCoords[2] += 1
+                blockCoords[4] = "V"
+
+        return blockCoords
+
+    def resultLeft(self, blockCoords):
+
+        if blockCoords[4] == "V":
+            blockCoords[1] -= 2
+            blockCoords[3] -= 1
+            blockCoords[4] = "H"
+        elif blockCoords[4] == "H":
+            if blockCoords[0] == blockCoords[2]:
+                blockCoords[1] -= 1
+                blockCoords[3] -= 2
+                blockCoords[4] = "V"
+            else:
+                blockCoords[1] -= 1
+                blockCoords[3] -= 1    
+
+        return blockCoords
+
+    def resultRight(self, blockCoords):
+
+        if blockCoords[4] == "V":
+            blockCoords[1] += 1
+            blockCoords[3] += 2
+            blockCoords[4] = "H"
+        elif blockCoords[4] == "H":
+            if blockCoords[0] == blockCoords[2]:
+                blockCoords[1] += 2
+                blockCoords[3] += 1
+                blockCoords[4] = "V"
+            else:
+                blockCoords[1] += 1
+                blockCoords[3] += 1
+
+        return blockCoords
+
+    def checkTogglers(self, state):
+
+        board = state.board
+        blockCoords = state.blockCoords
+        togglers = state.togglers
+
+        reservedLetters = ["X", "E", "O", "A"]
+
+        block1 = board[blockCoords[0]][blockCoords[1]]
+        block2 = board[blockCoords[2]][blockCoords[3]]
+
+        if block1.isupper() and block1 not in reservedLetters:
+            if block1 > "G" and blockCoords[4] == 'V' or block1 < "G":
+                togglers[block1] = not togglers[block1]
+        elif block2.isupper() and block2 not in reservedLetters:
+            if block2 > "G" and blockCoords[4] == 'V' or block2 < "G":
+                togglers[block2] = not togglers[block2]
+
+        return togglers
+
+    def result(self, state, action):
+
+        possibleActions = ['Up', 'Down', 'Left', 'Right']
+        resultCheckers = [self.resultUp, self.resultDown, self.resultLeft, self.resultRight]
+
+        state.blockCoords = resultCheckers[possibleActions.index(action)](state.blockCoords)
+        state.togglers = self.checkTogglers(state)
+
+        return state
+
+    def goal_test(self, state):
+
+        blockCoords = state.blockCoords
+        solutionCoords = state.solutionCoords
+
+        return True if blockCoords[4] == "V" and blockCoords[0] == solutionCoords[0] and blockCoords[1] == solutionCoords[1] else False
+
+
+    def isValid(self, state, i0, j0, i1, j1):
+
+        board = state.board
+        blockCoords = state.blockCoords
+        togglers = state.togglers
+
+        piece1 = board[blockCoords[0] +
+                             i0][blockCoords[1] + j0]
+        piece2 = board[blockCoords[2] +
+                             i1][blockCoords[3] + j1]
+
+        if piece1 in togglers:
+            if piece2 in togglers:
+                return togglers[piece1] and togglers[piece2]
+            return togglers[piece1]
+
+        if piece2 in togglers:
+            if piece1 in togglers:
+                return togglers[piece1] and togglers[piece2]
+            return togglers[piece2]
+
+        print(piece1)
+        if piece1 != "E" and piece2 != "E":
+            return True
+
+        return False
+
+    def validateUp(self, state):
+
+        blockCoords = state.blockCoords
+        board = state.board
+        togglers = state.togglers
+
+        if blockCoords[4] == "V":
+            if blockCoords[0] - 2 >= 0 and self.isValid(state, -2, 0, -1, 0):
+                return True
+        elif blockCoords[0] == blockCoords[2]:
+            if blockCoords[0] - 1 >= 0 and self.isValid(state, -1, 0, -1, 0):
+                return True
+        else:
+            piece = board[blockCoords[0] - 1][blockCoords[1]]
+            if piece in togglers:
+                return togglers[piece]
+
+            return True if blockCoords[0] - 1 >= 0 and piece != "E" else False
+
+        return False
+
+    def validateDown(self, state):
+
+        blockCoords = state.blockCoords
+        board = state.board
+        togglers = state.togglers
+
+        length = len(board)
+
+        if blockCoords[4] == "V":
+            if blockCoords[0] + 2 < length and self.isValid(state, 2, 0, 1, 0):
+                return True
+        elif blockCoords[0] == blockCoords[2]:
+            if blockCoords[0] + 1 < length and self.isValid(state, 1, 0, 1, 0):
+                return True
+        else:
+            piece = board[blockCoords[0] + 2][blockCoords[1]]
+            if piece in togglers:
+                return togglers[piece]
+
+            return True if blockCoords[0] + 1 < length and piece != "E" else False
+
+        return False
+
+    def validateLeft(self, state):
+
+        blockCoords = state.blockCoords
+        board = state.board
+        togglers = state.togglers
+
+        if blockCoords[4] == "V":
+            if blockCoords[1] - 2 >= 0 and self.isValid(state, 0, -2, 0, -1):
+                return True
+        elif blockCoords[0] == blockCoords[2]:
+            piece = board[blockCoords[0]][blockCoords[1] - 1]
+            if piece in togglers:
+                return togglers[piece]
+
+            return True if blockCoords[1] - 1 >= 0 and piece != "E" else False
+        else:
+            if blockCoords[1] - 1 >= 0 and self.isValid(state, 0, -1, 0, -1):
+                return True
+
+        return False
+
+    def validateRight(self, state):
+        
+        blockCoords = state.blockCoords
+        board = state.board
+        togglers = state.togglers
+
+        length = len(board[0])
+       
+        if blockCoords[4] == "V":
+            if blockCoords[3] + 2 < length and self.isValid(state, 0, 1, 0, 2):
+                return True
+        elif blockCoords[0] == blockCoords[2]:
+            piece = board[blockCoords[0]][blockCoords[1] + 2]
+            if piece in togglers:
+                return togglers[piece]
+
+            return True if blockCoords[3] + 1 < length and piece != "E" else False
+        else:
+            if blockCoords[3] + 1 < length and self.isValid(state, 0, 1, 0, 1):
+                return True
+
+        return False
+
+    def validate(self, action, state):
+        actions = ['Up', 'Down', 'Left', 'Right']
+        validators = [self.validateUp, self.validateDown, self.validateLeft, self.validateRight]
+
+        return validators[actions.index(action)](state)
+
