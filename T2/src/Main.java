10eddb3cfaa044cd59aebfb1d0aa23dde4b27d8c
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-05-04 20:38:56 +0100

    Added writeSolution()

--- T2/src/Main.java
+++ T2/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -6,13 +7,27 @@ public class Main {
 
         Reader.getInstance().readFile(args[0]);
 
-
-
         return;
     }
 
+    public static void writeSolution(ArrayList<String> solution, String filename) throws IOException {
+        FileOutputStream fstream = null;
 
+        try {
+            fstream = new FileOutputStream(filename + ".sln");
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fstream));
 
+        for (String exam : solution) {
+            System.out.println(exam);
+            bw.write(exam + "\r\n");
+        }
+
+        bw.close();
+    }
 
 }
